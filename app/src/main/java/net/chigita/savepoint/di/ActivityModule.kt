2038012dbcc88c563eedbfd9a41213767cfc9771
Kazuki Chigita <kazuki.chigichan@gmail.com>
2019-05-15 15:22:11 +0900

    define action switch to register fragment

--- app/src/main/java/net/chigita/savepoint/di/ActivityModule.kt
+++ app/src/main/java/net/chigita/savepoint/di/ActivityModule.kt
@@ -1,12 +1,18 @@
 package net.chigita.savepoint.di
 
 import androidx.fragment.app.FragmentActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.android.ContributesAndroidInjector
+import net.chigita.savepoint.R
 import net.chigita.savepoint.ui.activity.MainActivity
 import net.chigita.savepoint.ui.fragment.HomeFragment
 import net.chigita.savepoint.ui.fragment.HomeFragmentModule
+import net.chigita.savepoint.ui.fragment.RegisterFragment
+import net.chigita.savepoint.ui.fragment.RegisterFragmentModule
 
 /**
  * Created by chigichan24 on 2019-05-12.
@@ -22,6 +28,20 @@ abstract class ActivityModule {
       modules = [HomeFragmentModule::class]
   )
   abstract fun contributeHomeFragment(): HomeFragment
+
+  @ContributesAndroidInjector(
+      modules = [RegisterFragmentModule::class]
+  )
+  abstract fun contriubteRegisterFragment(): RegisterFragment
+
+  @Module
+  companion object {
+    @Provides
+    @JvmStatic
+    fun provideNavController(mainActivity: MainActivity): NavController {
+      return Navigation.findNavController(mainActivity, R.id.nav_host_fragment)
+    }
+  }
 
   @Module
   abstract class MainActivityBuilder {
--- app/src/main/java/net/chigita/savepoint/ui/fragment/HomeFragment.kt
+++ app/src/main/java/net/chigita/savepoint/ui/fragment/HomeFragment.kt
@@ -6,17 +6,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Lifecycle
+import androidx.navigation.NavController
 import dagger.Module
 import dagger.Provides
 import dagger.android.support.DaggerFragment
 import net.chigita.savepoint.R
 import net.chigita.savepoint.databinding.FragmentHomeBinding
+import javax.inject.Inject
 
 /**
  * Created by chigichan24 on 2019-05-14.
  */
 class HomeFragment : DaggerFragment() {
   private lateinit var binding: FragmentHomeBinding
+  @Inject lateinit var navController: NavController
 
   override fun onCreateView(
       inflater: LayoutInflater,
@@ -30,6 +33,13 @@ class HomeFragment : DaggerFragment() {
         false
     )
     return binding.root
+  }
+
+  override fun onActivityCreated(savedInstanceState: Bundle?) {
+    super.onActivityCreated(savedInstanceState)
+    binding.fab.setOnClickListener {
+      navController.navigate(R.id.action_main_to_register)
+    }
   }
 }
 
