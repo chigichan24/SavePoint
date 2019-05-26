48ec409790c8df7dd4d8ccfe6a0511fbee00e133
Kazuki Chigita <kazuki.chigichan@gmail.com>
2019-05-27 03:21:38 +0900

    ADd companion object that create new model instance

--- app/src/main/java/net/chigita/savepoint/vo/Angle.kt
+++ app/src/main/java/net/chigita/savepoint/vo/Angle.kt
@@ -3,6 +3,7 @@ package net.chigita.savepoint.vo
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import java.util.UUID
 
 /**
  * Created by chigichan24 on 2019-05-24.
@@ -10,7 +11,7 @@ import androidx.room.PrimaryKey
 @Entity(
     foreignKeys = [
       ForeignKey(
-          entity =Thing::class,
+          entity = Thing::class,
           parentColumns = ["uuid"],
           childColumns = ["thingUuid"],
           onDelete = ForeignKey.CASCADE
@@ -25,4 +26,28 @@ data class Angle(
     val x: Double,
     val y: Double,
     val z: Double
-)
+) {
+  companion object {
+    fun new(
+        thingUuid: String,
+        name: String,
+        description: String?,
+        x: Double,
+        y: Double,
+        z: Double
+    ): Angle {
+      if (name.isEmpty()) {
+        throw IllegalStateException("cannot allow blank name")
+      }
+      return Angle(
+          UUID.randomUUID().toString(),
+          thingUuid,
+          name,
+          description ?: "",
+          x,
+          y,
+          z
+      )
+    }
+  }
+}
