3472adc33158b797b7ab71c23e2f3f63f156e471
Kazuki Chigita <kazuki.chigichan@gmail.com>
2019-05-27 18:39:02 +0900

    Implement feature that measure angle

--- app/src/main/java/net/chigita/savepoint/ui/fragment/AngleRegisterFragment.kt
+++ app/src/main/java/net/chigita/savepoint/ui/fragment/AngleRegisterFragment.kt
@@ -1,6 +1,12 @@
 package net.chigita.savepoint.ui.fragment
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
@@ -22,11 +28,17 @@ import javax.inject.Inject
 /**
  * Created by chigichan24 on 2019-05-25.
  */
-class AngleRegisterFragment : Fragment(), Injectable {
+class AngleRegisterFragment : Fragment(), Injectable, SensorEventListener {
   private lateinit var binding: FragmentAngleRegisterBinding
   private val args by navArgs<AngleRegisterFragmentArgs>()
   @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
   lateinit var angleViewModel: AngleViewModel
+
+  private var mutableGeoMagneticList: MutableList<Float> = listOf(0.0f, 0.0f, 0.0f).toMutableList()
+  private var mutableAccelerationList: MutableList<Float> = listOf(0.0f, 0.0f, 0.0f).toMutableList()
+  private var x: Double = 0.0
+  private var y: Double = 0.0
+  private var z: Double = 0.0
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -49,6 +61,15 @@ class AngleRegisterFragment : Fragment(), Injectable {
 
   override fun onActivityCreated(savedInstanceState: Bundle?) {
     super.onActivityCreated(savedInstanceState)
+
+    // TODO: Fix me :(
+    // About sensor setup
+    val sensorManager = activity?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    val accelerationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+    val magneticSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
+    sensorManager.registerListener(this, accelerationSensor, SensorManager.SENSOR_DELAY_UI)
+    sensorManager.registerListener(this, magneticSensor, SensorManager.SENSOR_DELAY_UI)
+
     val args = AngleRegisterFragmentArgs.fromBundle(arguments!!)
     binding.rootConstraint.setOnTouchListener { v, _ ->
       v.requestFocus()
@@ -58,6 +79,7 @@ class AngleRegisterFragment : Fragment(), Injectable {
     binding.measureButton.setOnClickListener {
       binding.lottieAnim.visibility = View.VISIBLE
       binding.lottieAnim.playAnimation()
+      updateOrientations()
     }
   }
 
@@ -67,21 +89,60 @@ class AngleRegisterFragment : Fragment(), Injectable {
 
   override fun onOptionsItemSelected(item: MenuItem): Boolean {
     if (item.itemId == R.id.menu_completed) {
-      (args.thingUuid)?.let {
-        angleViewModel.registerAngle(
-            it,
-            binding.thingEditText.text.toString(),
-            binding.angleDescriptionEditText.text.toString(),
-            1.0,
-            1.0,
-            1.0
-        )
-        navigateToThingRegisterPage()
-      }
+      angleViewModel.registerAngle(
+          args.thingUuid,
+          binding.thingEditText.text.toString(),
+          binding.angleDescriptionEditText.text.toString(),
+          x,
+          y,
+          z
+      )
+      Log.d("Log", "$x $y $z")
+      navigateToThingRegisterPage()
       return false
     }
     return true
   }
 
   fun navigateToThingRegisterPage() = findNavController().popBackStack()
+
+  override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+
+  override fun onSensorChanged(event: SensorEvent?) {
+    when (event?.sensor?.type) {
+      Sensor.TYPE_MAGNETIC_FIELD -> {
+        mutableGeoMagneticList = event.values.clone().toMutableList()
+      }
+      Sensor.TYPE_ACCELEROMETER -> {
+        mutableAccelerationList = event.values.clone().toMutableList()
+      }
+    }
+  }
+
+  private fun updateOrientations() {
+    val R = FloatArray(MATRIX_SIZE)
+    val I = FloatArray(MATRIX_SIZE)
+    val O = FloatArray(MATRIX_SIZE)
+    val orientation = FloatArray(MATRIX_SIZE)
+    SensorManager.getRotationMatrix(
+        R,
+        I,
+        mutableAccelerationList.toFloatArray(),
+        mutableGeoMagneticList.toFloatArray()
+    )
+    SensorManager.remapCoordinateSystem(
+        R,
+        SensorManager.AXIS_X,
+        SensorManager.AXIS_Z,
+        O
+    )
+    SensorManager.getOrientation(O, orientation)
+    x = Math.floor(Math.toDegrees(orientation[0].toDouble()))
+    y = Math.floor(Math.toDegrees(orientation[1].toDouble()))
+    z = Math.floor(Math.toDegrees(orientation[2].toDouble()))
+  }
+
+  companion object {
+    const val MATRIX_SIZE = 9
+  }
 }
